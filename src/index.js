d0695e95437adb3f63522f968bd09ddc3f2b2f14
Cláudio Rapôso <engcfraposo@gmail.com>
2022-04-08 09:38:41 -0300

    feature:create initial questions

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync');
+
+const run = {
+    //DONE: Add a function for include term to searchTerm
+    _askAndReturnSearchTerm(){
+        return readline.question("Type a Wikipedia search term:");
+    },
+    //DONE: Add a function to ask and return prefix term
+    _askAndReturnPrefixTerm(){
+        const prefixes = ["Who is", "What is", "The history of"];
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option");
+        return prefixes[selectedPrefixIndex];
+    },
+    init(){
+        const content = {};
+        content.searchTerm = run._askAndReturnSearchTerm();
+        content.prefix = run._askAndReturnPrefixTerm();
+        console.log(content)
+    }
+}
+
+run.init();
